1195c0891bdddca60eff568ab6826b222b0206fe
Alex Armes <45469986+FruityBread@users.noreply.github.com>
2022-04-04 21:02:45 +0100

    testing multiplication

--- Main.java
+++ Main.java
@@ -2,14 +2,28 @@ public class Main{
     public static void main(String args[]){
 
         Matrix matrix1 = new Matrix(new float[][] {
-            {1,2},
-            {3,4}
+            {1,4,2},
+            {2,3,2},
+            {3,4,2}
         });
 
         Matrix matrix2 = new Matrix(new float[][] {
-            {1,2,3},
-            {4,5,6}
+            {7,2,2},
+            {2,7,2},
+            {1,6,6}
         });
+
+        System.out.println(matrix1.multiply(matrix2));
+
+        // Matrix matrix1 = new Matrix(new float[][] {
+            // {1,2},
+            // {3,4}
+        // });
+
+        // Matrix matrix2 = new Matrix(new float[][] {
+        //     {1,2,3},
+        //     {4,5,6}
+        // });
 
         // System.out.println(matrix1.add(matrix2, matrix2));
         
@@ -18,13 +32,13 @@ public class Main{
 
 
 
-        System.out.println(matrix1.multiply(matrix1, matrix2)); // this operation is equal to the following three operations
+        // System.out.println(matrix1.multiply(matrix1, matrix2)); // this operation is equal to the following three operations
 
-        Matrix matrix3 = matrix1.multiply(matrix2);
-        // System.out.println(matrix3);
+        // Matrix matrix3 = matrix1.multiply(matrix2);
+        // // System.out.println(matrix3);
 
-        System.out.println(matrix1.multiply(matrix3)); // 2x2 matrix can be multiplied by a 3x2 matrix
-        System.out.println(matrix3.multiply(matrix1)); // but a 3x2 cannot be multiplied by a 2x2
+        // System.out.println(matrix1.multiply(matrix3)); // 2x2 matrix can be multiplied by a 3x2 matrix
+        // System.out.println(matrix3.multiply(matrix1)); // but a 3x2 cannot be multiplied by a 2x2
 
     }
 }
